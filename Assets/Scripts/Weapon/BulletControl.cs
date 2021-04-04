ff3b258e31a069a042aa3e8d6f11f0481a610de1
teruteru634 <miyanagateru634@gmail.com>
2021-04-04 23:26:22 +0900

    ｔ

--- Assets/Scripts/Weapon/BulletControl.cs
+++ Assets/Scripts/Weapon/BulletControl.cs
@@ -6,6 +6,8 @@ namespace Musashi
 {
     public class BulletControl : MonoBehaviour
     {
+        [SerializeField] LayerMask targetLayer;
+        [SerializeField] float rayDistance;
         [SerializeField] float lifeTime = 1f;
         float timer = 0f;
         float shotPower;
@@ -20,12 +22,28 @@ namespace Musashi
         {
             timer += Time.deltaTime;
             transform.position += muzzle.forward * shotPower;
+
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, rayDistance, targetLayer))
+            {
+                if(hit.collider.TryGetComponent(out IDamageable target))
+                {
+                    Debug.Log("当たった");
+                    target.OnDamage(shotPower);
+                    Destroy(gameObject)S
+                }
+            }
+
             if (timer > lifeTime) 
             {
                 timer = 0f;
                 //gameObject.SetActive(false);
                 Destroy(gameObject);
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayDistance , Color.white);  
         }
     }
 }
