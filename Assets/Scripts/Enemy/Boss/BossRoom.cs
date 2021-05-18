26cbf59dd27c925e6ff3cb0a1a94e5b0ba2e07eb
teruteru634 <miyanagateru634@gmail.com>
2021-05-18 14:51:34 +0900

    ボス戦前のムービー作成に必要なパッケージのインポートと、スクリプトを実装中..

---
+++ Assets/Scripts/Enemy/Boss/BossRoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Musashi
+{
+    /// <summary>
+    /// ボス部屋のエントランスをプレイヤーが通過したら、
+    /// カットシーンを再生する
+    /// </summary>
+    public class BossRoom : MonoBehaviour
+    {
+        bool hasEntered;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (hasEntered) return;
+
+            if (other.CompareTag("Player"))
+            {
+                hasEntered = true;
+                Debug.Log("start cut movie");
+            }
+        }
+    }
+}
