810260649893848463353e1e8707fa065986d383
teruteru634 <miyanagateru634@gmail.com>
2021-06-02 10:23:15 +0900

    2体目の前敵のアセットをインポートした。

--- Assets/Scripts/Enemy/EnemyCounter.cs
+++ Assets/Scripts/Enemy/EnemyCounter.cs
@@ -1,78 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
 
-namespace Musashi
-{
-    public class EnemyCounter : MonoBehaviour
-    {
-        [SerializeField] TextMeshProUGUI enemyDefeatText;
-        [SerializeField] TextMeshProUGUI enemySpwanerText;
-        const string defeatText = "ENEMY DEFEAT :";
-        const string spwanerText = "Left Spwaner :";
+//namespace Musashi
+//{
+//    public class EnemyCounter : MonoBehaviour
+//    {
+//        [SerializeField] TextMeshProUGUI enemyDefeatText;
+//        [SerializeField] TextMeshProUGUI enemySpwanerText;
+//        const string defeatText = "ENEMY DEFEAT :";
+//        const string spwanerText = "Left Spwaner :";
 
-        private int defeatEnemyOfNumber;
-        private int leftSpwanerOfNumber;
-        public int DefeatEnemyOfNumber {
-            get { return defeatEnemyOfNumber; }
-            private set {
-                defeatEnemyOfNumber = value;
-                if(enemyDefeatText)
-                    enemyDefeatText.text = defeatText + DefeatEnemyOfNumber.ToString();
-            }
-        }
-        public int LeftSpwanerOfNumber {
-            get {return leftSpwanerOfNumber; }
-            private set { 
-                leftSpwanerOfNumber = value;
-                if (enemySpwanerText)
-                    enemySpwanerText.text = spwanerText + LeftSpwanerOfNumber.ToString();
-            }
-        }
+//        private int defeatEnemyOfNumber;
+//        private int leftSpwanerOfNumber;
+//        public int DefeatEnemyOfNumber {
+//            get { return defeatEnemyOfNumber; }
+//            private set {
+//                defeatEnemyOfNumber = value;
+//                if(enemyDefeatText)
+//                    enemyDefeatText.text = defeatText + DefeatEnemyOfNumber.ToString();
+//            }
+//        }
+//        public int LeftSpwanerOfNumber {
+//            get {return leftSpwanerOfNumber; }
+//            private set { 
+//                leftSpwanerOfNumber = value;
+//                if (enemySpwanerText)
+//                    enemySpwanerText.text = spwanerText + LeftSpwanerOfNumber.ToString();
+//            }
+//        }
 
-        bool hasEndGame = false;
+//        bool hasEndGame = false;
 
-        private void Start()
-        {
-            DefeatEnemyOfNumber = 0;
-            LeftSpwanerOfNumber = GameManager.Instance.SumOfEnemySpwaner;
-        }
+//        private void Start()
+//        {
+//            DefeatEnemyOfNumber = 0;
+//            LeftSpwanerOfNumber = GameManager.Instance.SumOfEnemySpwaner;
+//        }
 
-        private void UpdateEnemyCounter()
-        {
-            if (hasEndGame) return;
-            DefeatEnemyOfNumber++;
-        }
+//        private void UpdateEnemyCounter()
+//        {
+//            if (hasEndGame) return;
+//            DefeatEnemyOfNumber++;
+//        }
 
-        private void UpdateSpwanerCounter()
-        {
-            if (hasEndGame) return;
-            LeftSpwanerOfNumber--;
-            if (LeftSpwanerOfNumber == 0)
-            {
-                GameManager.Instance.GameClear();
-                hasEndGame = true;
-            }
-        }
+//        private void UpdateSpwanerCounter()
+//        {
+//            if (hasEndGame) return;
+//            LeftSpwanerOfNumber--;
+//            if (LeftSpwanerOfNumber == 0)
+//            {
+//                GameManager.Instance.GameClear();
+//                hasEndGame = true;
+//            }
+//        }
 
 
-        private void OnEnable()
-        {
-            GameEventManager.Instance.Subscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
-            //GameEventManager.Instance.Subscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
-        }
+//        private void OnEnable()
+//        {
+//            GameEventManager.Instance.Subscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
+//            //GameEventManager.Instance.Subscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
+//        }
 
-        private void OnDisable()
-        {
-            GameEventManager.Instance.UnSubscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
-            //GameEventManager.Instance.UnSubscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
-        }
+//        private void OnDisable()
+//        {
+//            GameEventManager.Instance.UnSubscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
+//            //GameEventManager.Instance.UnSubscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
+//        }
 
-        private void OnDestroy()
-        {
-            GameEventManager.Instance.UnSubscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
-            //GameEventManager.Instance.UnSubscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
-        }
-    }
-}
+//        private void OnDestroy()
+//        {
+//            GameEventManager.Instance.UnSubscribe(GameEventType.EnemyDie, UpdateEnemyCounter);
+//            //GameEventManager.Instance.UnSubscribe(GameEventType.SpawnDie, UpdateSpwanerCounter);
+//        }
+//    }
+//}
