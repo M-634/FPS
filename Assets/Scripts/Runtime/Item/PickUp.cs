8d5bd855bf7b70cacf607f1b48ea0fe147cbdc95
teruteru634 <miyanagateru634@gmail.com>
2021-07-21 00:54:16 +0900

    pickUpアイテムの動きをDotweenで実装

--- Assets/Scripts/Runtime/Item/PickUp.cs
+++ Assets/Scripts/Runtime/Item/PickUp.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DG.Tweening;
 
 namespace Musashi.Item
 {
     /// <summary>
-    /// フィールド上に落ちているアイテムの動きを制御するクラス
+    /// フィールド上に落ちているアイテムの動きを制御するクラス。
     /// </summary>
     public class PickUp : MonoBehaviour, IInteractable
-    {    
+    {
         public event Action<Transform> OnPickEvent;
+
+        [SerializeField] float rotateDuration = 1f;
+        [SerializeField] bool doRotate = true;
+
+        private void Start()
+        {
+            if (doRotate)
+            {
+                //rotate based world axix Y 
+                transform.DORotate(Vector3.up * 360, rotateDuration, RotateMode.WorldAxisAdd)
+                    .SetEase(Ease.Linear)
+                    .SetLoops(-1, LoopType.Incremental);
+            }
+        }
 
         public void Excute(Transform player)
         {
